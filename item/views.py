6c9ba0d18d84f7469b168a618110b7542d369293
anil7620 <anil76201reddy@gmail.com>
2023-09-19 22:23:36 -0500

    added details page

--- item/views.py
+++ item/views.py
@@ -4,6 +4,8 @@ from .models import Item
 # Create your views here.
 def detail(request, pk):
     item=get_object_or_404(Item, pk=pk)
+    related_items=Item.objects.filter(catergory=item.catergory).exclude(pk=pk)[:4]
     return render(request, 'item/detail.html',{
         'item':item,
+        'related_items':related_items,
     })
